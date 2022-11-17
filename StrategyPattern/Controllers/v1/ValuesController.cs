76b3dfad8aacb91852b199ba3e497b02696641da
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2022-11-17 14:26:29 -0300

    <Build>Folder/Controllers

---
+++ StrategyPattern/Controllers/v1/ValuesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrategyPattern.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
